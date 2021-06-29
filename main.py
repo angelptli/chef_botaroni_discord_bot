01d099de01870115efe5acbcf6e53e0cbc4f5e1b
angelptli <ali95@mail.ccsf.edu>
2021-06-28 20:03:49 -0700

    Implement inspire command

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import discord
 import os
 import re
+import requests
+import json
 
 client = discord.Client()
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return quote
 
 @client.event
 async def on_ready():
@@ -15,5 +23,7 @@ async def on_message(message):
 
     if re.search('[H|h]([a|e]llo|i|ey|owdy)', message.content):
         await message.channel.send('Hello to you too!')
+    elif message.content.startswith('$inspire'):
+        await message.channel.send(get_quote())
 
 client.run(os.getenv('TOKEN'))

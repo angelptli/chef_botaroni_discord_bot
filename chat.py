f66aa5a4b0a3b14809c16c301e21674a47a50135
angelptli <ali95@mail.ccsf.edu>
2021-06-28 18:41:48 -0700

    Let bot respond to author messages

--- chat.py
+++ chat.py
@@ -9,8 +9,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    # if message.author == client.user:
+    #     return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello to you too!')
